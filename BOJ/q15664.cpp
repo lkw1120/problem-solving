15327d0ae797955a064a2e7dc3dbba3714e34f59
lkw1120 <lkwkang@gmail.com>
2022-05-31 12:36:19 +0900

    https://www.acmicpc.net/problem/15664
    N과 M (10)

---
+++ BOJ/q15664.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[9] = {0};
+int input[9] = {0};
+bool isUsed[9] = {false};
+set<vector<int>> output;
+int N,M;
+void solve(int m, int n) {
+	if(m > M) {
+		vector<int> v;
+		for(int i=1;i<=M;i++) {
+			v.push_back(arr[i]);
+		}
+		output.insert(v);
+		return ;
+	}
+	for(int i=n;i<=N;i++) {
+		if(!isUsed[i]) {
+			arr[m] = input[i];
+			isUsed[i] = true;
+			solve(m+1,i);
+			isUsed[i] = false;
+		}
+	}
+}
+bool compare(int a, int b) { return a < b; }
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin>>N>>M;
+	for(int i=1;i<=N;i++) {
+		cin>>input[i];
+	}
+	sort(input,input+N+1,compare);
+	solve(1,1);
+	for(auto list: output) {
+		for(int i=0;i<list.size();i++) {
+			cout<<list[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
