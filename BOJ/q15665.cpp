b3a0b8a5b6b275f1e5a94af2ad0c118d4269d0c7
lkw1120 <lkwkang@gmail.com>
2022-06-01 14:11:18 +0900

    https://www.acmicpc.net/problem/15665
    N과 M (11)

---
+++ BOJ/q15665.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[9] = {0};
+int input[9] = {0};
+vector<int> v;
+int N,M;
+void solve(int m) {
+	if(m > M) {
+		for(int i=1;i<=M;i++) {
+			cout<<arr[i]<<" ";
+		}
+		cout<<"\n";
+		return ;
+	}
+	for(int i=0;i<v.size();i++) {
+		arr[m] = v[i];
+		solve(m+1);
+	}
+}
+bool compare(int a, int b) { return a < b; }
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin>>N>>M;
+	for(int i=1;i<=N;i++) {
+		cin>>input[i];
+	}
+	sort(input,input+N+1,compare);
+	for(int i=1;i<=N;i++) {
+		if(input[i] != input[i-1]) {
+			v.push_back(input[i]);
+		}
+	}
+	solve(1);
+	return 0;
+}
