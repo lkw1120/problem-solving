41b40f1eae6d1ca97b528ce974d9ea03373e6189
lkw1120 <lkwkang@gmail.com>
2022-11-01 22:08:42 +0900

    https://www.acmicpc.net/problem/1013
    Contact

---
+++ BOJ/q1013.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int T;
+bool isSignal(string str) {
+	for(int i=0;i<str.size();i++) {
+		if(str[i] == '0') {
+			if(str.size() < i+2) {
+				return false;
+			}
+			if(str[i+1] == '0') {
+				return false;
+			}
+			i++;
+		}
+		else {
+			if(str.size() < i+4) return false;
+			if(str[i+1] == '1' || str[i+2] == '1') {
+				return false;
+			}
+			i++;
+			while(i < str.size() && str[i] == '0') i++;
+			if(i == str.size()) {
+				return false;
+			}
+			i++;
+			while(i < str.size() && str[i] == '1') {
+				if(i+2 < str.size() && str[i+1] == '0' && str[i+2] == '0') {
+					break;
+				}
+				i++;
+			}
+			i--;
+		}
+	}
+	return true;
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>T;
+	while(T--) {
+		string str;
+		cin>>str;
+		if(isSignal(str)) {
+			cout<<"YES"<<"\n";
+		}
+		else {
+			cout<<"NO"<<"\n";
+		}
+	}
+	return 0;	
+}
