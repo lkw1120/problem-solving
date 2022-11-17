dbfa919560b4bb362dc4d2afd86d9b2a14925291
lkw1120 <lkwkang@gmail.com>
2022-11-17 14:32:46 +0900

    https://www.acmicpc.net/problem/1669
    멍멍이 쓰다듬기

---
+++ BOJ/q1669.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int X,Y,N,ans;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>X>>Y;
+	if(X == Y) {
+		ans = 0;
+	}
+	else {
+		N = sqrt(Y-X);
+		ans = 2*N-1;
+		if(0 < Y-X-N*N && Y-X-N*N <= N) {
+			ans++;
+		}
+		if(N < Y-X-N*N && Y-X-N*N <= 2*N) {
+			ans+=2;
+		}
+	}
+	cout<<ans<<"\n";
+	return 0;	
+}
