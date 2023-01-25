d895a33d6eb79a1cf1e504e240b7c5e83364097f
lkw1120 <lkwkang@gmail.com>
2023-01-25 21:32:09 +0900

    https://www.acmicpc.net/problem/24444
    알고리즘 수업 - 너비 우선 탐색 1

---
+++ BOJ/q24444.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> v;
+vector<bool> visited;
+vector<int> res;
+int N,M,R,U,V;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin>>N>>M>>R;
+	v.resize(N+1);
+	visited.resize(N+1);
+	res.resize(N+1);
+	for(int i=0;i<M;i++) {
+		cin>>U>>V;
+		v[U].push_back(V);
+		v[V].push_back(U);
+	}
+	for(int i=1;i<=N;i++) {
+		sort(v[i].begin(),v[i].end());	
+	}
+	int t = 1;
+	queue<pair<int,int>> q;
+	q.push({R,t});
+	visited[R] = true;
+	while(!q.empty()) {
+		int now = q.front().first;
+		res[now] = q.front().second;
+		q.pop();
+		for(auto next: v[now]) {
+			if(visited[next]) continue;
+			visited[next] = true;
+			q.push({next,++t});
+		}
+	}
+	for(int i=1;i<=N;i++) {
+		cout<<res[i]<<"\n";	
+	}
+	return 0;	
+}
