ed6a40c7da22016e8cf96b4aabee655240b7bed5
lkw1120 <lkwkang@gmail.com>
2023-04-19 00:33:21 +0900

    https://www.acmicpc.net/problem/15242
    Knight

---
+++ BOJ/q15242.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dX[8] = {-2,-1,1,2,2,1,-1,-2};
+int dY[8] = {1,2,2,1,-1,-2,-2,-1};
+string A,B;
+int ans;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>A>>B;
+	int arr[8][8] = {0};
+	int a,b,c,d;
+	a = A[0]-'a';
+	b = A[1]-'1';
+	c = B[0]-'a';
+	d = B[1]-'1';
+	queue<pair<int,int>> q;
+	q.push({a,b});
+	while(!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		if(x == c && y == d) {
+			ans = arr[x][y];
+			break;
+		}
+		for(int i=0;i<8;i++) {
+			int dx = x+dX[i];
+			int dy = y+dY[i];
+			if(0 <= dx && dx < 8 && 0 <= dy && dy < 8 && !arr[dx][dy]) {
+				arr[dx][dy] = arr[x][y]+1;
+				q.push({dx,dy});
+			}
+		}
+	}
+	cout<<ans<<"\n";
+	return 0;
+}
