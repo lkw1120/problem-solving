1522aa049ca19ef2303f5f771debdfe1bb16c732
lkw1120 <lkwkang@gmail.com>
2023-07-05 00:13:57 +0900

    https://www.acmicpc.net/problem/1365
    꼬인 전깃줄

---
+++ BOJ/q1365.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[100001] = {0};
+int dp[100001] = {0};
+int N,ans;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>N;
+	for(int i=1;i<=N;i++) {
+		cin>>arr[i];	
+	}
+	int len = 1;
+	dp[1] = arr[1];
+	for(int i=2;i<=N;i++) {
+		if(dp[len] < arr[i]) {
+			dp[++len] = arr[i];	
+		}
+		else {
+			auto idx = lower_bound(dp+1,dp+len+1,arr[i])-dp;
+			dp[idx] = arr[i];
+		}
+	}
+	ans = N-len;
+	cout<<ans<<"\n";
+	return 0;
+}
