eab424fbf740c868d478053eaad9b33376dcdafd
lkw1120 <lkwkang@gmail.com>
2024-05-27 04:03:29 +0900

    https://www.acmicpc.net/problem/12021
    보물 찾기

---
+++ BOJ/q12021.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+double A,B;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>A>>B;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<sqrt(A*B)<<" "<<sqrt(A*B)<<"\n";
+    return 0;
+}
