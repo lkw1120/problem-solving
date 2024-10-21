1b027091602d09f9e4af5dad6857bd30f56000ac
lkw1120 <lkwkang@gmail.com>
2024-10-22 01:11:31 +0900

    https://www.acmicpc.net/problem/30620
    서로소 싫어

---
+++ BOJ/q30620.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll X,Y;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>X>>Y;
+    cout<<2<<"\n";
+    cout<<X*Y-X<<"\n";
+    cout<<Y-X*Y<<"\n";
+    return 0;
+}
