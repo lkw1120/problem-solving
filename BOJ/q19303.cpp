6ac5f5068c27a2561c76b6db804f9a35735473a2
lkw1120 <lkwkang@gmail.com>
2024-12-03 00:08:30 +0900

    https://www.acmicpc.net/problem/19303
    Aftermath

---
+++ BOJ/q19303.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll A,H,N;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>A>>H;
+    N = A*H;
+    cout<<N<<"\n";
+    return 0;
+}
