6dbe8f634a8023f13d83c74bfea64d6ba1816b88
lkw1120 <lkwkang@gmail.com>
2025-02-03 00:59:14 +0900

    https://www.acmicpc.net/problem/12107
    약수 지우기 게임 1

---
+++ BOJ/q12107.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin>>N;
+    if(N == 1) {
+        cout<<"B"<<"\n";
+    } 
+    else {
+        cout<<"A"<<"\n";
+    }
+    return 0;
+}
