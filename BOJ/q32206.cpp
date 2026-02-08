4dbcb50744ca4a4f68b8160de75d9dd430bff011
lkw1120 <lkwkang@gmail.com>
2026-02-09 00:04:35 +0900

    https://www.acmicpc.net/problem/32206
    아보와 킨텍스

---
+++ BOJ/q32206.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+string str;
+int N,ans;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N>>str;
+    cout<<25*N+26<<"\n";
+    return 0;
+}
