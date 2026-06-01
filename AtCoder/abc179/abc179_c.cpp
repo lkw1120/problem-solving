4613650958c9d3e04acb43f2900516e73cef0f06
kwlee <lkwkang@gmail.com>
2026-06-01 17:45:22 +0900

    https://atcoder.jp/contests/abc179/tasks/abc179_c
    C - A x B + C

---
+++ AtCoder/abc179/abc179_c.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll N,ans;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N;
+    ans = 0;
+    for(int i=1;i<N;i++) {
+        ans+=(N-1)/i;
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
