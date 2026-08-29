0d0ed3196fe01cb02f2c634a95ad301b881f5f44
kwlee <lkwkang@gmail.com>
2026-08-29 15:48:58 +0900

    https://atcoder.jp/contests/abc186/tasks/abc186_d
    D - Sum of difference

---
+++ AtCoder/abc186/abc186_d.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+vector<ll> v;
+ll N,ans;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N;
+    v.resize(N);
+    for(int i=0;i<N;i++) {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    ll sum = 0;
+    for(int i=0;i<N;i++) {
+        ans+=(v[i]*i-sum);
+        sum+=v[i];
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
